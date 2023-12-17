77632e38298d835b57cb5583989833e808e8cf36
SathyaKrish23 <=>
2023-12-17 11:11:08 +0530

    first commit

---
+++ login/script.js
@@ -0,0 +1,20 @@
+
+function passwordToggle(){
+  if( passwordInput.type == "password"){
+    passwordInput.type = "text";
+    eye.src = "./show.png";
+  }else{
+    passwordInput.type = "password"
+    eye.src = "./hide.png";
+  }
+}
+
+
+
+
+
+
+
+
+  
+  
